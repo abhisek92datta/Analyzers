dcd9aaa69a31c79d999955851ca9325a8ffae912
abhisek92datta <abdatta@cern.ch>
2016-06-14 10:51:34 -0400

    Update CU_ttH_EDA_Misc.cc

--- ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
@@ -313,12 +313,17 @@ void CU_ttH_EDA::Check_Fill_Print_single_lepton(CU_ttH_EDA_event_vars &local)
 	int met_passed = 1;
 	float met_pt = 0;
 	float met_phi = 0;
-	fprintf(events_single_lepton, "%.4f, %.4f, %.4f, ", local.run_nr, local.lumisection_nr, local.event_nr);	
+	int pdgId;
+	fprintf(events_single_lepton, "%d, %d, %d, ", local.run_nr, local.lumisection_nr, local.event_nr);	
 	fprintf(events_single_lepton, "%d, %d, ", is_SL, is_DL);
-	if (local.n_electrons == 1)
-		fprintf = (events_single_lepton, "%.4f, %.4f, %.4f, %.4f, %.4f, ", local.e_selected[0].pt(), local.e_selected[0].eta(), local.e_selected[0].phi(), local.e_selected[0].isoDeposit(EcalIso), local.e_selected[0].pdgId() );
-	if (local.n_muons == 1)
-		fprintf = (events_single_lepton, "%.4f, %.4f, %.4f, %.4f, %.4f, ", local.mu_selected[0].pt(), local.mu_selected[0].eta(), local.mu_selected[0].phi(), local.mu_selected[0].isoDeposit(EcalIso), local.mu_selected[0].pdgId() );
+	if (local.n_electrons == 1) {
+		pdgId = 11;
+		fprintf(events_single_lepton, "%.4f, %.4f, %.4f, %.4f, %d, ", local.e_selected[0].pt(), local.e_selected[0].eta(), local.e_selected[0].phi(), local.e_selected[0].trackIso(), pdgId);
+	}
+	if (local.n_muons == 1) {
+		pdgId = -13;
+		fprintf(events_single_lepton, "%.4f, %.4f, %.4f, %.4f, %d, ", local.mu_selected[0].pt(), local.mu_selected[0].eta(), local.mu_selected[0].phi(), local.mu_selected[0].trackIso(), pdgId);
+	}
 	fprintf(events_single_lepton, "0, 0, 0, 0, 0, 0, 0, ");
 	fprintf(events_single_lepton, "%.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f, ", local.jets_selected_sorted[0].pt(), local.jets_selected_sorted[1].pt(), local.jets_selected_sorted[2].pt(), local.jets_selected_sorted[3].pt(), local.jets_selected_sorted[0].bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags"), local.jets_selected_sorted[1].bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags"), local.jets_selected_sorted[2].bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags"), local.jets_selected_sorted[3].bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags") );
 	fprintf(events_single_lepton, "%.4f, %.4f, %d, %d, %d, %d, %d, ", met_pt, met_phi, met_passed, local.n_jets, local.n_btags, b_weight, ttH_cat);
