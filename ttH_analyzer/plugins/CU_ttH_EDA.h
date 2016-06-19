9d64d90717218bd32167cdb3cb939ca639a4f2cf
abhisek92datta <abdatta@cern.ch>
2016-06-19 19:04:02 -0400

    Update CU_ttH_EDA.h

--- ttH_analyzer/plugins/CU_ttH_EDA.h
+++ ttH_analyzer/plugins/CU_ttH_EDA.h
@@ -173,6 +173,7 @@ class CU_ttH_EDA : public edm::EDAnalyzer
 	void SetFactorizedJetCorrector(const sysType::sysType iSysType=sysType::NA);
 	FactorizedJetCorrector* _jetCorrector;
 	JetCorrectionUncertainty* _jetCorrectorUnc;
+	double getJERfactor( const int, const double, const double, const double );
 	
 	/// Taggers. Returns 1 in case of an error
 	//int Higgs_tagger(Handle<boosted::SubFilterJetCollection>,
