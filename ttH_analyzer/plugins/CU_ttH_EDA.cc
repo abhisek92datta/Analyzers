53c4ca5160dfac3cddd120462f0d8d8ac0ca631b
abhisek92datta <abdatta@cern.ch>
2016-06-21 10:06:02 -0400

    Update CU_ttH_EDA.cc

--- ttH_analyzer/plugins/CU_ttH_EDA.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA.cc
@@ -280,18 +280,18 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	local.jets_selected = removeOverlapdR(local.jets_selected, local.e_selected, 0.4);
 	//local.jets_selected = removeOverlapdR(local.jets_selected, local.tau_selected, 0.4);
 
-	local.jets_selected_tag = miniAODhelper.GetSelectedJets(
+	local.jets_selected_tag_old = miniAODhelper.GetSelectedJets(
 		local.jets_corrected, min_bjet_pT, max_bjet_eta, jetID::jetTight,
 		MAODHelper_b_tag_strength);
 	
 	local.b_weight = 1;
 	
-	//for (const auto& jet : local.jets_selected_tag_old) {
-	//	if (miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags") > 0.89) {
-	//		local.b_weight = local.b_weight + miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags");
-	//		local.jets_selected_tag.push_back(jet);
-	//	}
-	//}
+	for (const auto& jet : local.jets_selected_tag_old) {
+		if (miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags") > 0.89) {
+			local.b_weight = local.b_weight * miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags");
+			local.jets_selected_tag.push_back(jet);
+		}
+	}
 		
 	local.n_jets = static_cast<int>(local.jets_selected.size());
 	local.n_btags = static_cast<int>(local.jets_selected_tag.size());
