4242f4f44ebe2b9dd5c4bb0029145687f77255f0
abhisek92datta <abdatta@cern.ch>
2016-06-22 17:53:54 -0400

    Update CU_ttH_EDA.cc

--- ttH_analyzer/plugins/CU_ttH_EDA.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA.cc
@@ -129,8 +129,8 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	local.pass_elemu = false;
 	Update_common_vars(iEvent, local);
 
-	if (local.event_nr != 329848 && local.event_nr != 918493 && local.event_nr != 922677 && local.event_nr != 923498 && local.event_nr != 1086517 && local.event_nr != 1094598 && local.event_nr != 1095976 && local.event_nr != 2844525 && local.event_nr != 2846224 && local.event_nr != 2991228 && local.event_nr != 3152405 && local.event_nr != 3892446 && local.event_nr != 3896127 && local.event_nr != 276186 && local.event_nr != 277594 && local.event_nr != 278270 && local.event_nr != 311560 && local.event_nr != 340532 )
-		return;
+	//if (local.event_nr != 329848 && local.event_nr != 918493 && local.event_nr != 922677 && local.event_nr != 923498 && local.event_nr != 1086517 && local.event_nr != 1094598 && local.event_nr != 1095976 && local.event_nr != 2844525 && local.event_nr != 2846224 && local.event_nr != 2991228 && local.event_nr != 3152405 && local.event_nr != 3892446 && local.event_nr != 3896127 && local.event_nr != 276186 && local.event_nr != 277594 && local.event_nr != 278270 && local.event_nr != 311560 && local.event_nr != 340532 )
+	//	return;
 	
 
 	/// Create and set up edm:Handles in stack mem.
@@ -352,7 +352,7 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 		if (local.n_prim_V > 0) {
 			if (local.n_leptons == 1) {
 				if (local.n_electrons == 1) {
-					if (local.n_veto_electrons == 1 && local.pass_single_e == 1) {
+					if (local.n_veto_electrons == 1 && local.n_veto_muons == 0 && local.pass_single_e == 1) {
 					//if (local.e_selected[0].eta() < max_ele_eta) {
 						if (local.n_jets >= min_njets && local.n_btags >= min_nbtags) {
 							local.event_selection = true;
@@ -360,7 +360,7 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 					}
 				}	
 				else if (local.n_muons == 1) {
-					if (local.n_veto_muons == 1 && local.pass_single_mu == 1) {
+					if (local.n_veto_muons == 1 && local.n_veto_electrons == 0 && local.pass_single_mu == 1) {
 					//if (local.mu_selected[0].eta() < max_mu_eta) {
 						if (local.n_jets >= min_njets && local.n_btags >= min_nbtags) {
 							local.event_selection = true;
@@ -371,6 +371,7 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 		}
 	}
 	
+	/*
 	std::cout<<"\n";
 	std::cout<<local.event_nr<<"   "<<local.n_leptons<<"   "<<local.n_electrons<<"   "<<local.n_veto_electrons<<"  "<<local.n_muons<<"  "<<local.n_veto_muons<<"   "<<local.n_jets<<"   "<<local.n_btags;
 	std::cout<<"\n";
@@ -381,6 +382,7 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 		std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
 	}
 	std::cout<<"\n";
+	*/
 	
 	if(local.n_leptons==1) {
 		//std::cout<<local.n_prim_V<<"  "<<local.n_leptons<<"  "<<local.n_muons<<"  "<<local.pass_single_mu<<"  "<<"  "<<local.n_electrons<<"  "<<local.pass_single_e<<"  "<<local.n_jets<<"  "<<local.n_btags<<"  "<<local.event_selection<<"\n";
