fb9efdfbcd9a6d9bfee095137cb9a70c14584159
Abhisek Datta <abdatta@cern.ch>
2016-06-24 14:25:33 -0400

    Update CU_ttH_EDA_Misc.cc

--- ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
@@ -1397,7 +1397,7 @@ CU_ttH_EDA::getCSVWeight(std::vector<double> jetPts, std::vector<double> jetEtas
 }
 
 void CU_ttH_EDA::getbweight (CU_ttH_EDA_event_vars &local) {
-	int iSys = 0; // none - 0,  JESUp - 7 , JESDown - 8
+	//int iSys = 0; // none - 0,  JESUp - 7 , JESDown - 8
 	double csvWgtHF, csvWgtLF, csvWgtCF;
 	
   	for( std::vector<pat::Jet>::const_iterator iJet = local.jets_selected.begin(); iJet != local.jets_selected.end(); iJet++ ){ 
@@ -1406,9 +1406,12 @@ void CU_ttH_EDA::getbweight (CU_ttH_EDA_event_vars &local) {
     	 	 local.vec_jet_csv.push_back(iJet->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"));
     		 local.vec_jet_hadronFlavour.push_back(iJet->hadronFlavour());
 	 } 
-
-	local.b_weight = getCSVWeight(local.vec_jet_pt, local.vec_jet_eta, local.vec_jet_csv,
+	std::cout<<local.vec_jet_pt.size()<<"  "<<local.vec_jet_eta.size()<<"  "<<local.vec_jet_csv.size()<<"  "<<local.vec_jet_hadronFlavour.size()<<"\n";
+	
+	local.b_weight=0;
+	//local.b_weight = getCSVWeight(local.vec_jet_pt, local.vec_jet_eta, local.vec_jet_csv,
                        local.vec_jet_hadronFlavour, iSys, csvWgtHF, csvWgtLF, csvWgtCF);
+          
 }
 
 
