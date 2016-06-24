49761c13960ee61382746c90cbf3e5c639a08ad4
Abhisek Datta <abdatta@cern.ch>
2016-06-24 15:55:22 -0400

    Update CU_ttH_EDA_Misc.cc

--- ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
@@ -1321,7 +1321,7 @@ void CU_ttH_EDA::getbweight (CU_ttH_EDA_event_vars &local) {
 	 } 
 	
 	//local.b_weight=0;
-	local.b_weight = csvhelper->getCSVWeight(local.vec_jet_pt, local.vec_jet_eta, local.vec_jet_csv, local.vec_jet_hadronFlavour, iSys, csvWgtHF, csvWgtLF, csvWgtCF);
+	local.b_weight = csvhelper.getCSVWeight(local.vec_jet_pt, local.vec_jet_eta, local.vec_jet_csv, local.vec_jet_hadronFlavour, iSys, csvWgtHF, csvWgtLF, csvWgtCF);
         //if (local.b_weight >= 1)
         //	local.b_weight = 1;
 }
