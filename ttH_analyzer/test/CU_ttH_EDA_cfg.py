16dc673e7cf7697544fb7aa26a7947f3c296bef8
Abhisek Datta <abdatta@cern.ch>
2016-06-27 19:45:22 -0400

    Update CU_ttH_EDA_cfg.py

--- ttH_analyzer/test/CU_ttH_EDA_cfg.py
+++ ttH_analyzer/test/CU_ttH_EDA_cfg.py
@@ -14,8 +14,8 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
 #process.GlobalTag.globaltag = '76X_mcRun2_asymptotic_v12'
-#process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4'
-process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_miniAODv2'
+process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4'
+#process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_miniAODv2'
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
@@ -28,8 +28,8 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
 	fileNames = cms.untracked.vstring(
         #htobb
-        '/store/mc/RunIISpring16MiniAODv2/ttHTobb_M125_13TeV_powheg_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v2/90000/0204C78B-9434-E611-B3B0-02163E014C6F.root'
-        #'/store/mc/RunIISpring15MiniAODv2/ttHTobb_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/30000/DA1B6FD6-C46D-E511-9C7B-00A0D1EE29B8.root'
+        #'/store/mc/RunIISpring16MiniAODv2/ttHTobb_M125_13TeV_powheg_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v2/90000/0204C78B-9434-E611-B3B0-02163E014C6F.root'
+        '/store/mc/RunIISpring15MiniAODv2/ttHTobb_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/30000/DA1B6FD6-C46D-E511-9C7B-00A0D1EE29B8.root'
         # tt+jet 
         #'/store/mc/RunIISpring15MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/0EE7E064-BE6F-E511-BB41-E4115BB4C4BC.root'
        	)
