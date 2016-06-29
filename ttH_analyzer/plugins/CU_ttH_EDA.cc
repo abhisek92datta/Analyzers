800f1d4f024ac7e7eb986fa84f6443f0b2c0a8ff
Abhisek Datta <abdatta@cern.ch>
2016-06-29 12:10:52 -0400

    Update CU_ttH_EDA.cc

--- ttH_analyzer/plugins/CU_ttH_EDA.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA.cc
@@ -366,7 +366,7 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	for ( auto& jet : local.jets_sl_raw) {
 		std::cout<<jet.pt()<<"\n";
 	}
-	for ( auto& jet : local.jets_sl_JEC_corrected) {
+	for ( auto& jet : local.jets_sl_corrected_JEC) {
 		std::cout<<jet.pt()<<"   ";
 		jet.setP4(jet.correctedJet(0).p4());
 		std::cout<<jet.pt()<<"\n";
