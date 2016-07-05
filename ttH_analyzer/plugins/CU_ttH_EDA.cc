7564a90de12c77a69252e24e47894cb8fc325e6b
Abhisek Datta <abdatta@cern.ch>
2016-07-04 22:29:19 -0400

    Update CU_ttH_EDA.cc

--- ttH_analyzer/plugins/CU_ttH_EDA.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA.cc
@@ -358,16 +358,17 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	Check_DL_Event_Selection(local);
 	
 	
-	std::cout<<"\n";
+	std::cout<<"\n\n\n";
 	std::cout<<local.event_nr<<"\n";
 	for ( auto& mu : *(handle.muons)) {
 		std::cout<<mu.pt()<<"  "<<mu.eta()<<"  "<<miniAODhelper.GetMuonRelIso(mu,coneSize::R04, corrType::deltaBeta)<<"  "<<miniAODhelper.passesMuonPOGIdTight(mu)<<"\n";
 	}
 	std::cout<<"\n";
 	std::cout<<local.n_di_electrons<<"  "<<local.n_di_muons<<"  "<<local.n_di_jets<<"   "<<local.n_di_btags<<"\n";
-	//for ( auto& jet : local.jets_sl_corrected) {
-	//	std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
-	//}
+	std::cout<<"\n";
+	for ( auto& jet : local.jets_sl_corrected) {
+		std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
+	}
 	//std::cout<<local.met_pt<<"  "<<local.mll<<"\n";
 
 	if (local.event_selection_SL!=0 || local.event_selection_DL!=0){
