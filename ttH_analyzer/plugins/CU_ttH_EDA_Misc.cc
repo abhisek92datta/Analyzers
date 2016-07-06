3775bf6ad75eb20ca5fa2484a3c29698898676a9
Abhisek Datta <abdatta@cern.ch>
2016-07-06 10:11:44 -0400

    Update CU_ttH_EDA_Misc.cc

--- ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA_Misc.cc
@@ -1087,12 +1087,14 @@ CU_ttH_EDA::GetJetSF( pat::Jet jet, const sysType::sysType iSysType, double rho)
 			unc = _jetCorrectorUnc->getUncertainty(false);
 			jes = 1 - unc;
       		}
-      		//scale = scale*jes;
+      		scale = scale*jes;
       		jet.scaleEnergy( jes );
-      		return (1/scale)*jes;
+      		//return (1/scale)*jes;
+      		return scale;
     	}
     	else
-    		return 1/scale;
+    		return scale;
+    		//return 1/scale;
     	/*
     	if( jet.genJet()) {
     		if ( iSysType == sysType::NA )
