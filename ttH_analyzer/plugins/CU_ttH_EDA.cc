e9af28c4796ba59a3e5a67bc761c2f3bba116c2e
Abhisek Datta <abdatta@cern.ch>
2016-07-06 10:39:06 -0400

    Update CU_ttH_EDA.cc

--- ttH_analyzer/plugins/CU_ttH_EDA.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA.cc
@@ -133,8 +133,8 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	local.pass_elemu = false;
 	Update_common_vars(iEvent, local);
 	
-	//if (local.event_nr != 1839 )
-	//	return;
+	if (local.event_nr != 1410845 )
+		return;
 	
 	/// Create and set up edm:Handles in stack mem.
 	edm_Handles handle;
@@ -385,21 +385,23 @@ void CU_ttH_EDA::analyze(const edm::Event &iEvent,
 	
 	std::cout<<"\n\n\n";
 	std::cout<<local.event_nr<<"\n";
-	for ( auto& jet : local.jets_raw) {
+	for ( auto& jet : *handle.jets) {
 		std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
 	}
-	//for ( auto& mu : *(handle.muons)) {
-	//	std::cout<<mu.pt()<<"  "<<mu.eta()<<"  "<<miniAODhelper.GetMuonRelIso(mu,coneSize::R04, corrType::deltaBeta)<<"  "<<miniAODhelper.passesMuonPOGIdTight(mu)<<"\n";
-	//}
-	//std::cout<<"\n";
-	//std::cout<<local.n_di_electrons<<"  "<<local.n_di_muons<<"  "<<local.n_di_jets<<"   "<<local.n_di_btags<<"\n";
 	std::cout<<"\n\n";
 	for ( auto& jet : local.jets_sl_corrected) {
 		std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
 	}
-	Fill_addn_quant(local, *rho, handle.genTtbarId);
 	std::cout<<"\n\n";
-	std::cout<<local.jet1SF_sl<<"  "<<local.jet1SF_up_sl<<"  "<<local.jet1SF_down_sl<<"\n";
+	for ( auto& jet : local.jets_sl_selected) {
+		std::cout<<jet.pt()<<"  "<<jet.eta()<<"  "<<miniAODhelper.GetJetCSV(jet,"pfCombinedInclusiveSecondaryVertexV2BJetTags")<<"\n";
+	}
+	
+		//for ( auto& mu : *(handle.muons)) {
+	//	std::cout<<mu.pt()<<"  "<<mu.eta()<<"  "<<miniAODhelper.GetMuonRelIso(mu,coneSize::R04, corrType::deltaBeta)<<"  "<<miniAODhelper.passesMuonPOGIdTight(mu)<<"\n";
+	//}
+	//std::cout<<"\n";
+	//std::cout<<local.n_di_electrons<<"  "<<local.n_di_muons<<"  "<<local.n_di_jets<<"   "<<local.n_di_btags<<"\n";
 	
 	//if(local.is_emu == false)
 	//	local.event_selection_DL = false;
