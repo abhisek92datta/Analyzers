c27f730acf50d8119727b18fc41e829e6625ee7a
Abhisek Datta <abdatta@cern.ch>
2016-07-20 19:18:23 -0400

    Update Analyzer_run_recipe.sh

--- Analyzer_run_recipe.sh
+++ Analyzer_run_recipe.sh
@@ -1,16 +1,7 @@
-cd CMSSW_8_0_8_patch1/src/
+cd CMSSW_8_0_12/src/
 git cms-addpkg PhysicsTools/JetMCAlgos
 cd PhysicsTools/
 git checkout CMSSW_8_0_X
 cd ../
 mv ../../Analyzers .
-mv Analyzers/GenTtbarCategorizer_cfi.py PhysicsTools/JetMCAlgos/python/
-mv Analyzers/GenTtbarCategorizer.cc PhysicsTools/JetMCAlgos/plugins/
-git clone https://github.com/abhisek92datta/MiniAOD.git
-cd MiniAOD
-git checkout CMSSW_8_0_8
-cd ..
-scram b -j 32
-scram b -j 32
-cd Analyzers/ttH_analyzer/
-cmsRun test/CU_ttH_mc_EDA_cfg.py 
+
