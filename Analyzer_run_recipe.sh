2e483d4ed46f40d60fd49d020071936911ab2e4a
Abhisek Datta <abdatta@cern.ch>
2016-07-20 19:20:47 -0400

    Update Analyzer_run_recipe.sh

--- Analyzer_run_recipe.sh
+++ Analyzer_run_recipe.sh
@@ -4,4 +4,5 @@ cd PhysicsTools/
 git checkout CMSSW_8_0_X
 cd ../
 mv ../../Analyzers .
-
+mv Analyzers/GenTtbarCategorizer_cfi.py PhysicsTools/JetMCAlgos/python/
+mv Analyzers/GenTtbarCategorizer.cc PhysicsTools/JetMCAlgos/plugins/
