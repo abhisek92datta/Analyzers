03a99d44f811e36035544e32a44f390d33b10490
Abhisek Datta <abdatta@cern.ch>
2016-07-24 02:10:42 -0400

    Update Pileup_calculation_script.sh

--- ttH_analyzer/macros/Pile_Up_Calc/Pileup_calculation_script.sh
+++ ttH_analyzer/macros/Pile_Up_Calc/Pileup_calculation_script.sh
@@ -1,3 +1,5 @@
+scram b -j 32
+
 python pileupCalc.py -i '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON.txt' --inputLumiJSON '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt' --calcMode 'true' --minBiasXsec 71300 --maxPileupBin 50 --numPileupBins 50  output.root
 
 root -l -q PU_data_hist_prod.C > PU_Data_temp.txt
