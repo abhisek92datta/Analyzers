04e2755cc8bf60e67314f6e719421c983b56e8d9
Abhisek Datta <abdatta@cern.ch>
2016-07-27 13:42:02 -0400

    Update Analyzer_run_recipe.sh

--- Analyzer_run_recipe.sh
+++ Analyzer_run_recipe.sh
@@ -13,3 +13,6 @@ git checkout CMSSW_8_0_8
 cd ..
 scram b -j 32
 scram b -j 32
+cd Analyzers/ttH_analyzer/macros/Pile_Up_Calc/
+chmod 755 Pileup_calculation_script.sh
+./Pileup_calculation_script.sh
