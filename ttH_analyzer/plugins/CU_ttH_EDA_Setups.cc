7793fac5b6ba92ec1d8b0b2b5ec04a8958b52ae9
Abhisek Datta <abdatta@cern.ch>
2016-08-17 11:10:51 -0400

    Update CU_ttH_EDA_Setups.cc

--- ttH_analyzer/plugins/CU_ttH_EDA_Setups.cc
+++ ttH_analyzer/plugins/CU_ttH_EDA_Setups.cc
@@ -159,8 +159,7 @@ void CU_ttH_EDA::Set_up_tokens(const edm::ParameterSet &config)
 	    config.getParameter<edm::InputTag>("jets"));
 	token.METs = consumes<pat::METCollection>(
 	    config.getParameter<edm::InputTag>("mets"));
-	if(!isdata)
-		token.genjets = consumes<reco::GenJetCollection>(
+	token.genjets = consumes<reco::GenJetCollection>(
 	    	config.getParameter<edm::InputTag>("genjets"));
 	token.PF_candidates = consumes<pat::PackedCandidateCollection>(
 	    config.getParameter<edm::InputTag>("pfcand"));
